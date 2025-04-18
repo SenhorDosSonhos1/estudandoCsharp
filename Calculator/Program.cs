5f7831036f375b48b60d0e854d74bae7c31b0c8f
SenhorDosSonhos1 <marcelosamu2002@gmail.com>
2025-04-18 16:18:47 -0300

    inicializando estudos

---
+++ Calculator/Program.cs
@@ -0,0 +1,111 @@
+﻿
+using System.Diagnostics.CodeAnalysis;
+
+static void Soma() {
+    Console.Clear();
+
+    Console.WriteLine("Primeiro valor: ");
+    float primeiroValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor: ");
+    float segundoValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+    Console.WriteLine($"{primeiroValor} + {segundoValor} = {primeiroValor + segundoValor}");
+
+    Console.ReadKey();
+    Menu();
+
+}
+
+static void Subtracao () {
+    Console.Clear();
+
+    Console.WriteLine("Primeiro valor: ");
+    float primeiroValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Primeiro valor: ");
+    float segundoValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    Console.WriteLine($"{primeiroValor} - {segundoValor} = {primeiroValor - segundoValor}");
+    Console.ReadKey();
+    Menu();
+
+}   
+
+static void Divisao () {
+    Console.Clear();
+
+    Console.WriteLine("Primeiro valor: ");
+    float primeiroValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor");
+    float segundoValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+
+    Console.WriteLine($"{primeiroValor} / {segundoValor} = {primeiroValor / segundoValor}");
+    
+    Console.ReadKey();
+    Menu();
+
+}
+
+static void Multiplicacao () {
+    Console.Clear();
+
+    Console.WriteLine("Primeiro valor");
+    float primeiroValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor");
+    float segundoValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine();
+
+    Console.WriteLine($"{primeiroValor} * {segundoValor} = {primeiroValor * segundoValor}");
+
+    Console.ReadKey();
+    Menu();
+
+}
+
+static void Menu () {
+    Console.Clear();
+
+    Console.WriteLine("O que deseja fazer?");
+    Console.WriteLine(" 1 - Soma");
+    Console.WriteLine(" 2 - Subtração");
+    Console.WriteLine(" 3 - Divisão");
+    Console.WriteLine(" 4 - Multiplicação");
+    Console.WriteLine(" 5 - Sair");
+
+    Console.WriteLine("---------------------------");
+    Console.WriteLine("Selecione uma opção: ");
+
+    short resultado = short.Parse(Console.ReadLine());
+
+    switch (resultado) {
+        case 1: Soma ();
+            break;
+
+        case 2: Subtracao ();
+            break;
+
+        case 3: Divisao ();
+            break;
+
+        case 4: Multiplicacao ();
+            break;
+
+        case 5: System.Environment.Exit(9);
+            break;
+        
+        default: Menu();
+            break;
+
+    }
+}
+
+Menu();
---
+++ StopWatch/Program.cs
@@ -0,0 +1,29 @@
+﻿static void Menu () {
+    Console.Clear();
+    Console.WriteLine("S = Segundo => 10s = 10 segundos");
+    Console.WriteLine("N = Minuto => 1m = 1 minuto");
+    Console.WriteLine("0 = Sair");
+    Console.WriteLine("Quanto tempo deseja contar? ");
+
+    string data = Console.ReadLine().ToUpper();
+    Console.WriteLine(data);
+}
+
+static void Start (int timer) {
+    int currentTime = 0;
+
+    while (timer > currentTime) {
+        Console.Clear();
+
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+    }
+
+    Console.Clear();
+    Console.WriteLine("StopWatch finalizado...");
+    Thread.Sleep(2500);
+}
+
+//Start(6);
+Menu();
